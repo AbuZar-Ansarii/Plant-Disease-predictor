20eb127ac0c6129ce9b5602b20ae3160c3c7fba3
AbuZar-Ansarii <venomdemon718@gmail.com>
2025-02-09 11:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.markdown("<div class='image-container'>", unsafe_allow_html=True)  # Start of image container
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     st.markdown("</div>", unsafe_allow_html=True)  # End of image container
     # button
     if st.button("Predict Disease"):
